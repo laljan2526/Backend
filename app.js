b1ac01aa99c02f69921b0dca21e811fb9c2b5430
laljan2526 <63613459+laljan2526@users.noreply.github.com>
2023-12-22 23:11:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res)=> {
+
+    res.send('LaljanMDotcom')
+})
+
+
+app.get('/login', (req, res)=> {
+
+  res.send('<h1>You are Loged in now</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
